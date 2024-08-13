4a77fa1f1787b77deba1a48396e63cee106104d2
millalinden <milla.linden@hyperisland.se>
2024-08-13 14:27:36 +0200

    updated favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -65,7 +65,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
-        <link rel="icon" href="/images/ullsta1.svg" />
+        <link rel="icon" href="../../public/icons/ullsta1.svg" />
       </Head>
       <body className="bg-[#FFFDFA]">{children}</body>
     </html>
