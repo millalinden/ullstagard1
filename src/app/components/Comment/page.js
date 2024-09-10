c9155f1b4c1e036727c2f3556ce24d722ff2c77d
millalinden <milla.linden@hyperisland.se>
2024-09-10 11:59:11 +0200

    chnaged comment

--- src/app/components/Comment/page.js
+++ src/app/components/Comment/page.js
@@ -1,37 +1,3 @@
-// "use client";
-// import React, { useEffect, useRef } from "react";
-// import useContentful from "@/app/utils/useContentful";
-// import { useState } from "react";
-// import CommentItem from "../CommentItem/page";
-
-
-// export default function Comment() {
-//   const { getGuestbookComment } = useContentful();
-//   const [comments, setComments] = useState([]);
-
-//   useEffect(() => {
-//     const fetchComments = async () => {
-//       try {
-//         const commentsData = await getGuestbookComment();
-//         console.log('Fetched Comments:', commentsData); // Debugging log
-//         setComments(commentsData || []);
-//       } catch (error) {
-//         console.error("Error fetching comments:", error);
-//       }
-//     };
-  
-//     fetchComments();
-//   }, [getGuestbookComment]);
-  
-//   return (
-//     <>
-//       {comments.map((comment, index) => (
-//         <CommentItem key={index} comment={comment} />
-//       ))}
-//     </>
-//   );
-// }
-
 "use client";
 import React, { useEffect, useRef } from "react";
 import useContentful from "@/app/utils/useContentful";
