e55257f63fe7543949d62ae58f10613ef37f82df
millalinden <milla.linden@hyperisland.se>
2024-09-11 10:48:50 +0200

    updated metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,34 +1,6 @@
 "use client";
 import localFont from "next/font/local";
 import "./globals.css";
-import Head from "next/head";
-
-// Importing Erode font
-const erode = localFont({
-  src: [
-    {
-      path: "../../public/font/Erode-Variable.ttf",
-      weight: "300",
-    },
-    {
-      path: "../../public/font/Erode-Variable.ttf",
-      weight: "400",
-    },
-    {
-      path: "../../public/font/Erode-Variable.ttf",
-      weight: "500",
-    },
-    {
-      path: "../../public/font/Erode-Variable.ttf",
-      weight: "600",
-    },
-    {
-      path: "../../public/font/Erode-Variable.ttf",
-      weight: "700",
-    },
-  ],
-  variable: "--font-erode",
-});
 
 // Importing Satoshi font
 const satoshi = localFont({
@@ -84,20 +56,21 @@ const cabinet = localFont({
   variable: "--font-cabinet",
 });
 
+export const metadata = {
+  title: 'Ullsta Gård',
+  description:
+    'Ullsta Gård i Gåsinge socken ligger mitt emellan Gnesta och Mariefred i vacker sörmländsk natur. Gården ägs av familjen Lindén sedan 1927 och är idag en viktig del i familjens fritid med perfekt närhet till Stockholm.',
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="sv">
-      <Head>
-        <title>Ullsta Gård</title>
-        <meta
-          name="description"
-          content="Ullsta Gård i Gåsinge socken ligger mitt emellan Gnesta och Mariefred i vacker sörmländsk natur. Gården ägs av familjen Lindén sedan 1927 och är idag en viktig del i familjens fritid med perfekt närhet till Stockholm."
-        />
+      <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="icon" href="/favicon.ico" sizes={32} />
-      </Head>
+        <link rel="icon" href="/favicon.ico" sizes="32x32" />
+      </head>
       <body
-        className={`bg-[#FFFDFA] ${erode.variable} ${satoshi.variable} ${cabinet.variable}`}
+        className={`bg-[#FFFDFA] ${satoshi.variable} ${cabinet.variable}`}
       >
         {children}
       </body>
