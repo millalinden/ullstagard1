9f6ac7d959f5af14a9c323f4e8bcb4c1a2714c29
millalinden <milla.linden@hyperisland.se>
2024-09-11 10:37:13 +0200

    History page started

--- src/app/(pages)/history/page.js
+++ src/app/(pages)/history/page.js
@@ -3,8 +3,9 @@ import Layout from "@/app/components/Header/layout";
 export default function History() {
   return (
     <Layout>
-      <section>
-        <h2>Historia</h2>
+      <section className="w-screen h-screen font-cabinet text-[4vw] flex flex-col justify-center items-center">
+        <h2 className="">Historia</h2>
+        <p>Kommer snart!</p>
       </section>
     </Layout>
   );
